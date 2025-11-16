0937e13f7b83ee2ed42639f4130df561616fe441
MartinatorTime <martinatortime@gmail.com>
2025-11-16 15:52:08 +0200

    update Dockerfile and start_cloudflared.sh

--- scripts/start_cloudflared.sh
+++ scripts/start_cloudflared.sh
@@ -2,7 +2,7 @@
 if [[ "$IS_PRIVILEGED" == "true" ]]; then
     sysctl -w net.core.rmem_max=8388608
     sysctl -w net.core.wmem_max=8388608
-    cloudflared tunnel --autoupdate-freq "24h" --protocol "quic" --loglevel "info" run --token "$CF_TOKEN" --url http://localhost:80
+    cloudflared tunnel --autoupdate-freq "24h" --protocol "quic" --loglevel "info" run --token "$CF_TOKEN"
 else
-    cloudflared tunnel --autoupdate-freq "24h" --protocol "http2" --loglevel "info" run --token "$CF_TOKEN" --url http://localhost:80
+    cloudflared tunnel --autoupdate-freq "24h" --protocol "http2" --loglevel "info" run --token "$CF_TOKEN"
 fi
